60c78bd51715fe808432d0cab974321575b984c1
saigenix <sainathmahindrakar@gmail.com>
2022-03-16 16:16:47 +0530

    nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './Navbar';
 function App() {
   return (
     <div className="App">
-     <p>Hello</p> 
+     <p>Hello</p>
+     <Navbar/>
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <>
+
+
+<div className="nav">
+  <input type="checkbox" id="nav-check"/>
+  <div className="nav-header">
+    <div className="nav-title">
+      Work_pro
+    </div>
+  </div>
+  <div className="nav-btn">
+    <label for="nav-check">
+      <span></span>
+      <span></span>
+      <span></span>
+    </label>
+  </div>
+  
+  <div className="nav-links">
+  <a href="/#" >Login/sign-up</a>
+    <a href="/#" >About us</a>
+    <a href="/#" >Contact us</a>
+    <a href="/#" >Privacy policy</a>
+    <a href="/#" >More Tools</a>
+  </div>
+</div>
+    </>
+  )
+}
